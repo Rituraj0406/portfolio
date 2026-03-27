23cea8645ead0aca87648209b3c791716cee2894
Rituraj0406 <riturajpiku01@gmail.com>
2026-03-27 14:34:09 +0530

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import './index.css';
-import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
 
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Portfolio/>
-      {/* <Home/> */}
     </div>
   );
 }
